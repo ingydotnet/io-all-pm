63b26f0e14dac3ea1dca259ac95a0411b3648564
Ingy dot Net <ingy@ingy.net>
2010-08-16 22:35:27 -0700

    v0.41

--- lib/IO/All.pm
+++ lib/IO/All.pm
@@ -6,7 +6,7 @@ require Carp;
 # So one can use Carp::carp "$message" - without the parenthesis.
 sub Carp::carp;
 use IO::All::Base -base;
-our $VERSION = '0.40';
+our $VERSION = '0.41';
 use File::Spec();
 use Symbol();
 use Fcntl;
