d2fe44c88b1752cdf663ae9bb8b29fb205b55c52
Nikolay Mishin <mi@ya.ru>
2014-10-02 00:27:25 +0400

    add method rows

--- lib/IO/All.pm
+++ lib/IO/All.pm
@@ -716,6 +716,13 @@ sub slurp {
     }
 }
 
+sub rows {
+    my $self = shift;
+    my @lines = map {chomp; $_} $self->slurp;
+	return scalar @lines;
+}
+
+
 sub utf8 {
     my $self = shift;
     if ($] < 5.008) {
